1fbe0562c4cc023d4d4344a4c6702e3bdfa5a74a
harsh <hanzo4679@gmail.com>
2025-06-01 12:03:13 +0530

    first commit

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,20 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "html",
+    "css",
+    "tsx",
+  },
+
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
